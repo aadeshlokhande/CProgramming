78f4bdd9160c20091d19b429418aab83f09772f3
Aadesh <aadeshlokhande11@gmail.com>
2024-03-01 11:43:57 +0530

    01 / 03 / 2024

---
+++ Hackify/bigTiger/String.c
@@ -0,0 +1,44 @@
+// String
+
+
+// #include<stdio.h>
+// int main()
+// {
+    // char name[20] = {'A', 'a', 'd', 'e', 's', 'h', '\0'};
+    // char name[20] = "Aadesh";
+    // printf("%c",name[2]);
+    // for(int i = 0; i<=5; i++)
+    // {
+    //     printf("%c\n",name[i]);
+    // }
+
+    // printf("my name is %s.",name);
+
+    // return 0;
+// }
+
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     char name[20], lname[20];
+//     printf("enter your name = ");
+//     scanf("%s%s", name, lname);
+
+//     printf("my name is %s %s",name, lname);
+//     return 0;
+// }
+
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     char str[50];
+//     printf("Enter a str = ");
+//     scanf("%[^\n]s",str);
+
+//     printf("my str = %s",str);
+//     return 0;
+// }

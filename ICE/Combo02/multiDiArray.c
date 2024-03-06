7e8c425f28d2da0b3e25d2620e75a2aa51992d94
Aadesh <aadeshlokhande11@gmail.com>
2024-03-06 18:07:11 +0530

    06 / 03 / 2024

--- ICE/Combo02/multiDiArray.c
+++ ICE/Combo02/multiDiArray.c
@@ -235,3 +235,23 @@
 
 // = = = = = = = = = = = = = 
 
+
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a = 0;
+
+//     for(int i = 1; i<5; i++)
+//     {
+//         for(int j = 1; j<=i; j++)
+//         {
+//             a++;
+//             printf("%d ", a);
+//         }
+//         printf("\n");
+//     }
+
+//     return 0;
+// }

282a16fdcb681c6a7b7b1d5b0d9b4fc1f97d251a
Aadesh <aadeshlokhande11@gmail.com>
2024-03-07 01:41:05 +0530

    07 / 03 / 2024

---
+++ ICE/Combo02/stringDemo.c
@@ -0,0 +1,52 @@
+// String 
+
+
+// #include<stdio.h>
+// int main()
+// {
+    // char name[20] = {'A','a','d','e','s','h','\0'};
+    // char name[20] = "Aadesh";
+
+    // printf("%c",name[0]);
+    // for(int i = 0; i<6; i++)
+    // {
+    //     printf("%c\n",name[i]);
+    // }
+    // printf("my name is %s.",name);
+
+    // char name[20], lname[20];
+    // printf("Enter a name = ");
+    // scanf("%s%s",name,lname);
+
+    // printf("hello %s %s..",name,lname);
+    
+//     char str[10];
+//     printf("enter a str = ");
+//     scanf("%[^\n]s",str);
+
+//     printf("str = %s",str);
+
+//     return 0;
+// }
+
+// ================================================
+
+// STRING function
+#include<string.h>
+#include<stdio.h>
+int main()
+{
+    int a;
+    char str[20] = "AadeSh";
+    // char str1[20];
+    // a = strlen(str);
+    // a = strcmp(str,str1);
+    // printf("%d",a);
+    
+    // strcat(str, str1);
+    // strcpy(str1,str);
+    // strlwr(str);
+    // strupr(str);
+    strrev(str);
+    printf("%s",str);
+}
---
+++ ICE/Gojo/multiDArray.c
@@ -0,0 +1,32 @@
+// multi-dimention Array
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     // int a[3][4] = {{2,3,4,5},{4,6,8,9},{4,3,6,8}};
+//     // printf("%d",a[1][2]);
+//     int a[3][4];
+//     for(int i=0; i<3; i++)
+//     {
+//         for(int j = 0; j<4; j++)
+//         {
+//             printf("enter a number = ");
+//             scanf("%d",&a[i][j]);
+//         }
+     
+//     }
+    
+//     for(int i=0; i<3; i++)
+//     {
+//         for(int j = 0; j<4; j++)
+//         {
+//             printf("%d ",a[i][j]);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// =============================================

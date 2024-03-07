33c27e5db99f23048ac91652d9090c3cc543ec28
Aadesh <aadeshlokhande11@gmail.com>
2024-03-07 19:51:06 +0530

    07 / 03 / 2024

---
+++ ICE/Gojo/string.c
@@ -0,0 +1,29 @@
+// STRING - collection of char 
+
+// #include<stdio.h>
+// int main()
+// {
+    // char name[20] = {'A','a','d','e', 's', 'h','\0'};
+    // printf("%c",name[3]);
+    // char name[20] = "Aadesh";
+    // for(int i = 0; i<=5; i++)
+    // {
+    //     printf("%c\n",name[i]);
+    // }
+    // printf("my name is %s.",name);
+
+
+    // char str[20], str1[20];
+    // printf("enter a str = ");
+    // scanf("%s%s", str, str1);
+    // printf("str = %s %s",str, str1);
+
+//     char str[20];
+//     printf("Enter a str = ");
+//     scanf("%[^\n]s",str);
+//     printf("str = %s",str);
+//     return 0;
+// }
+
+
+

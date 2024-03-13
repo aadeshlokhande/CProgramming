98379c3a81ae64f9fa82ab6216f742e7e43aa576
Aadesh <aadeshlokhande11@gmail.com>
2024-03-13 19:26:39 +0530

    13 / 03 / 2024

---
+++ ICE/Combo02/filehandling.c
@@ -0,0 +1,35 @@
+// file handling
+
+// read 
+// #include<stdio.h>
+// int main()
+// {
+//     char str[20];
+//     FILE *ptr;
+//     ptr = fopen("abc.txt", "r");
+//     fscanf(ptr, "%s",str );
+//     fclose(ptr);
+//     printf("str = %s",str);
+// }
+
+// write 
+// #include<stdio.h>
+// int main()
+// {
+//     FILE *ptr;
+//     // char str[20] = "hello guys";
+//     ptr = fopen("abcdef.txt", "w");
+//     fprintf(ptr, "hello mitro");
+//     fclose(ptr);
+// }
+
+// append
+
+// #include<stdio.h>
+// int main()
+// {
+//     FILE *ptr;
+//     ptr = fopen("abcdef.txt", "a");
+//     fprintf(ptr, "\nhello mitro ");
+//     fclose(ptr);
+// }

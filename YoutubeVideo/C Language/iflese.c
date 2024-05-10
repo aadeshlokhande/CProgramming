b261a520b1a60a28b05ebb59590162e871569b9e
Aadesh <aadeshlokhande11@gmail.com>
2024-05-10 16:09:03 +0530

    10 / 05 / 2024

---
+++ YoutubeVideo/C Language/iflese.c
@@ -0,0 +1,19 @@
+// If else 
+
+#include<stdio.h>
+int main()
+{
+    int age;
+    printf("Enter a age = ");
+    scanf("%d",&age);
+
+    if(age>18)
+    {
+        printf("you can drive");
+    }
+    else
+    {
+        printf("you cannt dirve");
+    }
+    return 0;
+}

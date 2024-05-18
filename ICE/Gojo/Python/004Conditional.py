342e0d83034fde380238c6f1dbedc41f3048ada8
Aadesh <aadeshlokhande11@gmail.com>
2024-05-18 12:48:08 +0530

    18 / 05 / 2024

---
+++ ICE/Gojo/Python/004Conditional.py
@@ -0,0 +1,81 @@
+# conditional statement
+
+# if 
+
+# num = 6
+# if(num < 10):
+#     print("Hello")
+#     print("Hello")
+#     print("Hello")
+
+
+# age = int(input("enter a age = "))
+
+# if(age>18):
+#     print("Adult ")
+
+# print("Boy") 
+# =====================
+
+# if else 
+# if condition:
+#     code 
+# else:
+#     code 
+
+
+# num = int(input("enter a number = "))
+
+# if(num>10):
+#     print("yes")
+# else:
+#     print("No")
+
+
+# age = int(input("enter your age = "))
+
+# if(age>18):
+#     print("you can vote")
+# else:
+#     print("you can't vote")
+
+# ****************************************************
+# TrueCode if condition else FalseCode
+# age = int(input("enter your age = "))
+# print("you can vote") if (age>18) else print("you can't vote")
+
+# ****************************************************
+
+# a = int(input("enter a number = "))
+# b = int(input("enter a number = "))
+
+# if (a > b):
+#     print(a, "is greater")
+# else:
+#     print(b,"is greater")
+# ==================================================
+
+# a = int(input("enter a number = "))
+# b = int(input("enter a number = "))
+# c = int(input("enter a number = "))
+
+# if(a>b and a>c):
+#     print(a,"is greater")
+
+# if(b>a and  b>c):
+#     print(b,"is greater")
+
+# if(c>a and c>b):
+#     print(c,"is greater")
+# ========================================
+
+
+# a = int(input("enter a number = "))
+
+# if(a>=0):
+#     print("+ve number")
+# else:
+#     print("-ve number")
+
+# ========================================
+
---
+++ ICE/Gojo/Python/005ifelseLadder.py
@@ -0,0 +1,51 @@
+# if else ladder 
+# |_|
+# |_|
+# |_|
+# |_|
+# |_|
+# |_|
+# |_|
+# | |
+
+# if(condition):
+#     code 
+# elif(condition):
+#     code
+# elif(condition):
+#     code
+# elif(condition):
+#     code
+# elif(condition):
+#     code
+# else:
+#     code 
+# =======================================
+
+# num = int(input("enter a number = "))
+
+# if(num==1):
+#     print("ONE")
+# elif(num==2):
+#     print("TWO")
+# elif(num==3):
+#     print("THREE")
+# elif(num==4):
+#     print("FOUR")
+# else:
+#     print("invalid number")
+
+# ==================
+
+# num = int(input("enter a number = "))
+# if(num>0):
+#     print("+ve")
+# elif(num<0):
+#     print("-ve")
+# else:
+#     print("zero")
+
+# ================================
+
+
+

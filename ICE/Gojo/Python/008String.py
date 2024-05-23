7e1c51fc879d8798f27aeaa1e668edf2636d55ff
Aadesh <aadeshlokhande11@gmail.com>
2024-05-23 18:28:56 +0530

    23 / 05 / 2024

---
+++ ICE/Gojo/Python/008String.py
@@ -0,0 +1,74 @@
+# String 
+
+# ****************** Slicing ******************
+
+# a = "Shila ki Jawani"
+# print(a)
+# print(a[4])
+# print(a[-4])
+# print(a[0:5])
+# print(a[-7:-1])
+# print(a[:5])
+# print(a[5:])
+# print(a[0:-1])
+# print(a[0:5:1])
+# print(a[0:5:2])
+# print(a[::2])
+# print(a[::-1])
+# print(a[-1:-8:-1])
+
+
+# ****************** methods ******************
+
+# str1 = "maTru ki bijLi ka manDola"
+
+# print(type(str1))
+# print(len(str1))
+# print(dir(str1))
+
+# print("original =",str1)
+# print(str1.upper())
+# print(str1.lower())
+# print(str1.capitalize())
+# print(str1.title())
+# print(str1.count(" "))
+# print(str1.find("ki"))
+# print(str1.index("ki"))
+# print(str1.swapcase())
+# print(str1.replace("ki", "lol"))
+# print(str1.split("i"))
+# print(str1.center(31))
+
+
+# ****************** join ******************
+
+# a = "abc"
+# b = " ".join(a)
+# print(b)
+
+# a = "as fd as gf sa gf sd"
+# b = a.split()
+# print(b)
+# a = " ".join(b)
+# print(a)
+
+# ============================
+# ****************** format ******************
+
+# name = "aadesh"
+# age = 76
+# a = "my name is {1} and my age is {0} year old".format(name, age)
+# print(a)
+
+
+
+# name = input("enter your name = ")
+# age = int(input("enter your age= "))
+# a = f"my name is {name} and my age is {age} year old"
+# print(a)
+
+# a = 8
+# ans = a ** 100 
+# print(ans)
+# =======================
+
---
+++ ICE/Gojo/Python/009list.py
@@ -0,0 +1,68 @@
+# list  ----> [] -----> mutable 
+
+# list = ["Tamater", "Aalu", "Gobi"] 
+
+# print(list)
+# print(type(list))
+# print(len(list))
+# print(list[1])
+
+# list.append("matar")
+
+# print(list)
+# list.insert(1, "pyaj")
+# print(list)
+
+# list2 = ["matar", "pyaj", "baigan"]
+# list.append(list2)
+# list.extend(list2)
+
+# print(list)
+
+# list.pop(4)
+# list.remove("pyaj")
+# print(list)
+
+# list.clear()
+# a = list.copy()
+# print(list)
+# print(a)
+
+# a = list
+# a = list.copy()
+# list.pop(0)
+
+# print(a)
+# print(list)
+
+# list.sort()
+# a = list.count("Aalu")
+# a = list.index("Aalu")
+# list.reverse()
+
+# print(a)
+
+# a = []
+
+# a.append(10)
+# a.append(13)
+# a.append(16)
+# print(a)
+
+# ========================
+
+# a = 10
+# b = 20 
+
+# print(a,b)
+
+# temp = a 
+# a = b 
+# b = temp
+
+# a = a + b 
+# b = a - b 
+# a = a - b 
+# a,b = b,a 
+# print(a,b)
+# =================================

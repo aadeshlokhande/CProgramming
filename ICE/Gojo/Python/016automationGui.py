2fc0b04999ffc5ea9e599a0fecfdbfa91a107c61
Aadesh <aadeshlokhande11@gmail.com>
2024-06-05 09:32:44 +0530

    05 / 06 / 2024

---
+++ ICE/Gojo/Python/016automationGui.py
@@ -0,0 +1,61 @@
+# automation
+
+# pip install pyautogui 
+
+# import pyautogui as py
+# from time import sleep 
+
+
+# keyboard
+# typing - kjhsdkjhyfynbds fds gd dfkjlufds
+# press - alt, ctrl
+# shortcut - 
+# py.click() 
+
+# a = "my name is aadesh"
+# py.typewrite(a,0.1)
+
+# py.press("enter")
+# py.hotkey("ctrl","a")
+# py.press("enter")
+# ========================================
+# mouse
+# click 
+# scroll 
+# double click 
+# right click
+# drag 
+# move 
+
+# a = py.position()
+# print(a)
+# py.moveTo(300,300,3)
+# py.move(300,300,3)
+# py.click(1645,289)
+# py.click()
+# py.rightClick()
+# py.scroll(300)
+
+# py.dragTo(300,300,2)
+# py.doubleClick()
+
+# ===========================================
+import pyautogui as pg
+from time import sleep 
+
+# a = pg.position()
+# print(a)
+# exit()
+pg.press("win")
+sleep(1)
+pg.typewrite("whatsapp",0.1)
+pg.press("enter")
+sleep(2)
+pg.typewrite("rohit c lang",0.2)
+sleep(3)
+pg.click(291,185)
+
+a = ["rohit... jaldi fees complete kro...","hello","rohit reply kro","fees ka msg dekh ke ignore mat kro"]
+for msg in a:
+    pg.typewrite(msg,0.1)
+    pg.press("enter")

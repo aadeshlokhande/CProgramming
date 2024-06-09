5394ee7cbaf3b1bc63258fa6c291b805373a3283
Aadesh <aadeshlokhande11@gmail.com>
2024-06-10 02:45:50 +0530

    10 / 06 / 2024

---
+++ ICE/Gojo/Python/018Function.py
@@ -0,0 +1,92 @@
+# FUNCTIONS -----> Block of code 
+
+# def FunctionName(arguments):
+#     code 
+#     return value 
+
+# def SUM(a,b):
+#     c = a + b 
+#     return c 
+
+# a = SUM(10,20)
+# print(a)
+# ========================================
+
+# def greet(name):
+#     print("Hello",name)
+
+# greet("Aadesh")
+# greet("prathmesh")
+# greet("mayur")
+# greet("rohit")
+# greet("anushka")
+# greet("vaishanavi")
+# greet("siddhi")
+# =====================================
+
+# def sum():
+#     a = int(input("enter a number = "))
+#     b = int(input("enter a number = "))
+#     c = a + b 
+#     print(c)
+
+# sum()
+# sum()
+# sum()
+
+# ======================================
+
+# with argument with return 
+# def Sum(a,b):
+#     c = a + b
+#     return c
+
+# ans = Sum(10,20)
+# print(ans)
+
+
+# with argument without return 
+
+# def Sum(a,b):
+#     ans = a + b 
+#     print(ans)
+
+
+# pop = Sum(10,20)
+# print(pop)
+
+
+# without argument with return 
+
+# def Sum():
+#     a = int(input("enter number ="))
+#     b = int(input("enter number =")) 
+#     c = a + b 
+#     return c 
+
+# ans = Sum()
+# print(ans)
+
+
+
+# without argument without return 
+# def Sum():
+#     a = 10
+#     b = 20
+#     c = a + b 
+#     print(c)
+
+# Sum()
+
+
+# ==================================================
+# lambda
+
+# Sum = lambda a,b : print(a + b) 
+
+# Sum(100,200)
+# =========================================
+
+# square = lambda a: a*a 
+# print(square(60))
+
---
+++ ICE/Gojo/Python/019Recursion.py
@@ -0,0 +1,33 @@
+# Recursion 
+
+# def Hello(a):
+#     if(a>=1):
+#         print("Hello",end=" ")
+#         Hello(a-1)
+
+# Hello(5)
+
+# ===================================
+
+# 5! = 5x4x3x2x1
+# 4! = 4x3x2x1 
+# 5! = 5x4! 
+# n! = n x (n-1)!
+
+# 1! = 1 
+# 0! = 1 
+
+
+# def factorial(a):
+#     if(a==1 or a==0):
+#         return 1 
+#     else:
+#         return a * factorial(a-1)
+
+# num = int(input("enter a value = "))
+# ans = factorial(num)
+# print(ans)
+
+# ==============================================
+# fibbonaci
+# 0,1,1,2,3,5,8,13,21,

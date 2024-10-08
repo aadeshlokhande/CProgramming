9d7c9cb47412f022a069713ecad46767908f270f
Aadesh <aadeshlokhande11@gmail.com>
2024-10-08 22:09:56 +0530

    08 / 10 / 2024

--- Symbosis/Sec D/oct003.c
+++ Symbosis/Sec D/oct003.c
@@ -11,3 +11,52 @@
 // {}{}{{}} ---> valid
 // {{}{{}{}}{}} ---> valid
 
+// '{' === ++a 
+// '}' ==== --a 
+// }{
+// a==0 ====> yes / no 
+
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int a = 0, len;
+    char str[50];
+
+    printf("Enter a str = ");
+    scanf("%s",str);
+
+    len = strlen(str);
+
+    for(int i = 0; i<len; i++)
+    {
+        if(a>=0)
+        {
+            if(str[i]=='{')
+            {
+                ++a;
+            }
+            else 
+            {
+                --a;
+            }
+        }
+        else 
+        {
+            break;
+        }
+    }
+    if(a==0)
+    {
+        printf("Yes");
+    }
+    else 
+    {
+        printf("No");
+    }
+
+
+    return 0;
+}

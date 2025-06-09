416c36ebcd2368b0458c972de413cd6ad5c8233d
Aadesh <aadeshlokhande11@gmail.com>
2025-06-09 19:54:58 +0530

    none

---
+++ CodeMate/June Codemate/001intro.c
@@ -0,0 +1,15 @@
+//  binary 
+// high voltage ---> 1 
+// low voltage -----> 0
+
+// hello ----> 100100101011000100101010010111010010001
+// compiler
+
+// 10 + 20 ----> compiler ----> binary
+// output in binary ------> compiler ----> human readable (30)
+
+// 10 / 3 = 3.333333
+// 10 / 3 = 3.333334 
+//  x 10000000000000
+
+
---
+++ CodeMate/June Codemate/002Variable.c
@@ -0,0 +1,88 @@
+// variable -- cantainer
+
+// a = 927438234
+// b = 927438924
+// c = 987439234
+// d = 982743982
+// e = 987234234
+
+
+// ==================================
+
+// data type 
+
+// int - %d -  12,43,65,34,-43,-65,0
+// float - %f - 7.5, 3.14, 5.8, 1.5 
+// char - %c - 'f', 'P', '0', '*' 
+
+
+// ==========================================
+
+// Identifier
+// a = 987342342
+// aadeshNum = 98324234234
+
+// 5star, 7sister, 21rollnumber, 
+// roll_number, first_name, my_age ,_name, name_
+
+// Return = 10
+
+// a = 10 
+// A = 20
+
+// age = 10
+// Age = 20
+// AGE = 30 
+
+// ================================================
+
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     // datatype variableName;
+//     // int a;
+    
+//     // variableName = value;
+//     // a = 10;
+    
+//     // datatype variableName = value;
+//     int a = 100;
+//     float b = 23.65;
+//     char c = 'h';
+    
+//     // printf("mere pass %d rs hai",a);
+//     printf("a = %d\n",a);
+//     printf("b = %f\n",b);
+//     printf("c = %c\n",c);
+// }
+
+
+// ==========================================
+
+
+// scanf()
+
+#include<stdio.h>
+int main()
+{
+    int age;
+    printf("enter your age = ");
+    scanf("%d", &age);
+    
+    printf("my age is %d years old.",age);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+

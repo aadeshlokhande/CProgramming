493c6a39dc5dca5cbffcedac7d98804bc064821a
Aadesh <aadeshlokhande11@gmail.com>
2025-06-12 18:33:00 +0530

    none

---
+++ CodeMate/June Codemate/005switch-Goto.c
@@ -0,0 +1,96 @@
+// **************** Switch case ****************
+
+// syntax
+// switch(variable)
+// {
+//     case value1:
+//         code 
+    
+//     case value2:
+//         code
+    
+//     case value3:
+//         code 
+//         .
+//         .
+//         .
+//     default:
+//         code
+// }
+
+
+// example
+
+// #include<stdio.h>
+// int main()
+// {
+//     int num;
+//     printf("Enter a number = ");
+//     scanf("%d",&num);
+
+//     switch(num)
+//     {
+//         case 1:
+//             printf("ONE\n");
+//             break;
+
+//         case 2:
+//             printf("TWO\n");
+//             break;
+
+//         case 3:
+//             printf("THREE\n");
+//             break;
+
+//         case 4:
+//             printf("FOUR\n");
+//             break;
+
+//         default:
+//             printf("out of range\n");
+
+//     }   
+//     printf("Hello");
+//     return 0;
+// }
+
+// #################################################
+// #################### HOMEWORK ###################
+// #################################################
+
+
+
+
+
+
+// =<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=
+// **************** GOTO Statements ****************
+
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Helllo Aadesh,\n");
+//     abc:
+//     printf("Good Morning,");
+//     goto abc;
+//     printf("Have a nice day\n");
+//     return 0;
+// }
+
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int num = 5;
+//     printf("Helllo Aadesh,\n");
+//     abc:
+//     printf("Good Morning,\n");
+//     if(num>1)
+//     {
+//         --num;
+//         goto abc;
+//     }
+//     printf("Have a nice day\n");
+//     return 0;
+// }

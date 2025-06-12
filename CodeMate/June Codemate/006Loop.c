5c5d462fd366452cf639f7af99ad3c89eeb0926f
Aadesh <aadeshlokhande11@gmail.com>
2025-06-12 19:14:07 +0530

    none

---
+++ CodeMate/June Codemate/006Loop.c
@@ -0,0 +1,78 @@
+// **************** LOOPS ****************
+
+// **************** FOR **************** 
+// syntax
+// for(start; end; inc/dec)
+// {
+//     code;
+// }
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a;
+//     for(a = 1; a<=10; a++)
+//     {
+//         printf("%d\n",a);
+//     }
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a;
+//     for(a = 10; a>=1; a--)
+//     {
+//         printf("%d\n",a);
+//     }
+//     return 0;
+// }
+
+// =<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=
+
+// **************** WHILE LOOP ****************
+// syntax
+// while(condition)
+// {
+//     code;
+// }
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a = 1;
+//     while (a<=10)
+//     {
+//         printf("Hello \n");
+//         a++;
+//     }
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int num = 6;
+
+//     while (num>=0)
+//     {
+//         printf("Enter a -ve number = ");
+//         scanf("%d",&num);
+//     }
+
+//     printf("Thanks for entring -ve number");
+//     return 0;
+// }
+
+
+
+
+
+
+// =<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=
+// do while 
+// nested for

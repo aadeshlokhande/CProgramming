f153998079b65666ab387ddbde4ffbe8e4c160d4
Aadesh <aadeshlokhande11@gmail.com>
2025-06-14 14:00:33 +0530

    none

--- CodeMate/June Codemate/006Loop.c
+++ CodeMate/June Codemate/006Loop.c
@@ -75,4 +75,259 @@
 
 // =<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=
 // do while 
-// nested for
+
+// do
+// {
+//     code;
+// }
+// while(condition);
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a;
+
+//     do{
+//         printf("Enter a -ve number = ");
+//         scanf("%d",&a);
+//     }while(a >= 0);
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int num1,num2,ans, con;
+//     do {
+//         printf("Enter a number = ");
+//         scanf("%d",&num1);
+//         printf("Enter a number = ");
+//         scanf("%d",&num2);
+//         ans = num1 + num2;
+//         printf("%d + %d = %d\n",num1,  num2, ans);
+
+//         printf("do you want to continue 1/0 = ");
+//         scanf("%d",&con);
+//     }while(con==1);
+
+//     return 0;
+// }
+
+
+
+
+// =<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=
+// *********** Nested For ******************
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     for(int i = 1; i<=5; i++)
+//     {
+//         printf("%d ",i);
+//     }
+//     return 0;
+// }
+
+
+// syntax
+
+// for(init; cond; ine/dec)
+// {
+//     code;
+//     for(init; cond; ine/dec)
+//     {
+//         code;
+//     }
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     for(int i = 1; i<=5; i++)
+//     {
+//         for (int j = 1; j<=5; j++)
+//         {
+//             printf("* ");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     for(int i = 1; i<=5; i++)
+//     {
+//         for (int j = 1; j<=5; j++)
+//         {
+//             printf("%d ",j);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// i:
+// 1, 2, 3, 4, 5
+
+// j: 
+// 1, 2, 3, 4, 5
+// 1, 2, 3, 4, 5
+// 1, 2, 3, 4, 5
+// 1, 2, 3, 4, 5
+// 1, 2, 3, 4, 5
+
+// output:
+// * * * * *
+// * * * * * 
+// * * * * * 
+// * * * * * 
+// * * * * * 
+
+// =<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=
+// #include<stdio.h>
+// int main()
+// {
+//     for(int i = 1; i<=5; i++)
+//     {
+//         for (int j = 1; j<=5; j++)
+//         {
+//             printf("%d%d ",i,j);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+// 11 12 13 14 15 
+// 21 22 23 24 25 
+// 31 32 33 34 35 
+// 41 42 43 44 45 
+// 51 52 53 54 55 
+
+// 1 1 1 1 1
+// 0 0 0 0 0 
+// 0 0 0 0 0 
+// 0 0 0 0 0 
+// 0 0 0 0 0 
+
+// #include<stdio.h>
+// int main()
+// {
+//     for (int i = 1; i<=5; i++)
+//     {
+//         for (int j = 1; j<=5; j++)
+//         {
+//             if(i==1)
+//             {
+//                 printf("1 ");
+//             }
+//             else 
+//             {
+//                 printf("0 ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+// =<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=
+
+// 11 12 13 14 15 
+// 21 22 23 24 25 
+// 31 32 33 34 35 
+// 41 42 43 44 45 
+// 51 52 53 54 55 
+
+// 1 0 0 0 0
+// 0 1 0 0 0 
+// 0 0 1 0 0 
+// 0 0 0 1 0 
+// 0 0 0 0 1 
+
+
+
+
+// 11               12 13 14 15 
+// 21 22            23 24 25 
+// 31 32 33         34 35 
+// 41 42 43 44      45 
+// 51 52 53 54 55 
+
+// 1 0 0 0 0 
+// 1 1 0 0 0 
+// 1 1 1 0 0 
+// 1 1 1 1 0 
+// 1 1 1 1 1
+
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+// #include<stdio.h>
+// int main()
+// {
+//     for (int i = 1; i<=5; i++)
+//     {
+//         for (int j = 1; j<=5; j++)
+//         {
+//             if(i>=j)
+//             {
+//                 printf("* ");
+//             }
+//             else 
+//             {
+//                 printf("  ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+// =<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=
+
+// #include<stdio.h>
+// int main()
+// {
+//     for(int i = 1; i<=5; i++)
+//     {
+//         for (int j=1; j<=i; j++)
+//         {
+//             printf("* ");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     for(int i = 1; i<=5; i++)
+//     {
+//         for (int j=1; j<=i; j++)
+//         {
+//             printf("%d ",j);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }

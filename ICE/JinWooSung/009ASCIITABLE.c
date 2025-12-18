61bd27b96bd1a6f97cc0bccdf10a500acee5c700
Adesh <aadeshlokhande11@gmail.com>
2025-12-18 20:39:15 +0530

    C Language

---
+++ ICE/JinWooSung/009ASCIITABLE.c
@@ -0,0 +1,33 @@
+// **************** ASCII TABLE ****************
+
+// ASCII ---> american standard code of information interchange
+// ANSI ---> american national standard institute
+
+
+// bin ---> dec 
+// dec---> oct 
+// oct --> hec dec 
+
+// 'A' ---> 
+// char ---> dec
+// 0-9
+
+// 1 2 4 8 16 32 64 128 256
+// 2^8
+// 128 --> +ve ---> ascii
+// 128 --> -ve ---> (not assign)
+
+// 7 '7'
+
+// #include<stdio.h>
+// int main()
+// {
+    // int a = 106;
+    // printf("a = %c",a);
+
+    // char a = 'A';
+    // char a = 70;
+    // printf("%c",a);
+
+//     return 0;
+// }

3c38f3950f27de5f449f4171cab011f4cba91c93
Adesh <aadeshlokhande11@gmail.com>
2025-12-19 20:25:30 +0530

    C Language

---
+++ ICE/JinWooSung/010DoWhileLoop.c
@@ -0,0 +1,36 @@
+// **************** Do while Loop ****************
+
+// while(condition)
+// {
+//     code 
+// }
+
+
+
+
+// syntax
+// do 
+// {
+//     code 
+// } while(condition);
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a,b,c,con;
+//     do 
+//     {
+//         printf("Enter a number = ");
+//         scanf("%d",&a);
+//         printf("Enter a number = ");
+//         scanf("%d",&b);
+//         c = a + b;
+//         printf("answer = %d\n",c);
+
+//         printf("do you want to continue = ");
+//         scanf("%d",&con);
+//     }while(con==1);
+
+//     return 0;
+// }
+
---
+++ ICE/JinWooSung/012String.c
@@ -0,0 +1,64 @@
+// **************** String ****************
+// collection of chars
+
+// "aadesh"
+// 'p'
+// "jvgjgkhkhkhPSDFSFD02374829387^%$^#$   ^$" ---> string
+// "p" --> string
+// 'po' ---> not allowed
+
+// name 
+// address 
+// email 
+// mobile 
+
+// #include<stdio.h>
+// int main()
+// {
+    // char arr[10] = {'A','a','d','e','s','h', '\0'};
+    // char arr[10] = "Aadesh";
+    
+    // for(int i = 0; i<6; i++)
+    // {
+    //     printf("%c -> ", arr[i]);
+    // }
+    // printf("my name is %s.", arr);
+//     return 0;
+// }
+// =<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=
+
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     char name[50];
+//     printf("Enter a name = ");
+//     scanf("%s", name);
+//     printf("my name is %s.",name);
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     char name[50], lname[50];
+//     printf("Enter a name = ");
+//     scanf("%s%s", name,lname);
+//     printf("my name is %s and last name is %s.",name,lname);
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     char name[50];
+//     printf("enter a name = ");
+//     // gets(name);
+//     scanf("%[^\n]s",name);
+//     printf("My name is %s.",name);
+//     return 0;
+// }
+ 

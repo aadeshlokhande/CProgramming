202fb95c3851ee23e7eb1324aa8ebae196605166
Adesh <aadeshlokhande11@gmail.com>
2025-12-26 20:04:20 +0530

    C Language

--- ICE/JinWooSung/0000gitupdate.c
+++ ICE/JinWooSung/0000gitupdate.c
@@ -1,12 +1,9 @@
 #include <stdlib.h>
-
-int main() 
-{    
+void main()
+{
     system("cd ..");
     system("cd ..");
-    system("git status");
     system("git add .");
     system("git commit -m \"C Language\"");
     system("git push -u origin main");
-    return 0;
 }
